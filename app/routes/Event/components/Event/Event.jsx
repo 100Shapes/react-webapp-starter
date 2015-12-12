6135cceb361a25c14a894a6919c9df65126d300f
Michele Memoli <michele@100shapes.com>
2015-12-12 01:11:01 +0000

    Simplifed dumb Event Component

--- app/routes/Event/components/Event/Event.jsx
+++ app/routes/Event/components/Event/Event.jsx
@@ -1,21 +1,17 @@
-import React, {Component, PropTypes} from 'react';
-/**
- * Event Component
- */
-export default class Event extends Component {
+import React, {PropTypes} from 'react';
 
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        subtitle: PropTypes.string.isRequired
-    };
+const Event = ({name, subtitle}) => {
+    return (
+        <div>
+            <h2 className="mb0">{name}</h2>
+            <p>{subtitle}</p>
+        </div>
+    );
+};
 
-    render() {
-        const {name, subtitle} = this.props;
-        return (
-            <div>
-                <h2 className="mb0">{name}</h2>
-                <p>{subtitle}</p>
-            </div>
-        );
-    }
-}
+Event.propTypes = {
+    name: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+};
+
+export default Event;
