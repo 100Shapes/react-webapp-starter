7d02a480779d08853f768e16d1011b37d5855fe6
Michele Memoli <michele@100shapes.com>
2015-12-12 01:20:43 +0000

    Added more event actions test coverage

--- app/modules/events/actions.test.js
+++ app/modules/events/actions.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import expect from 'expect.js';
 import * as actions from './actions';
+import * as constants from './constants';
 
 const {describe, it} = global;
 
@@ -8,6 +9,11 @@ describe('Module: events (actions)', () => {
 
     it('defines an startEvent()', () => {
         expect(actions.startEvent).to.be.a('function');
+    });
+
+    it('startEvent returns an action with EVENT_START type', () => {
+        expect(actions.startEvent())
+            .to.have.property('action', constants.EVENT_STARTED);
     });
 
 });
